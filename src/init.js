fbe7c6048c0e40412f9d3c09cd77fc9bdb964398
Vasilek <morphizm@ya.ru>
2020-01-26 17:47:09 +0300

    lint

--- src/init.js
+++ src/init.js
@@ -1,10 +1,8 @@
 import '@babel/polyfill';
 import 'bootstrap';
 import './custom.scss';
-import Example from './Example';
+// import Example from './Example';
 
 export default () => {
-  const element = document.getElementById('container');
-  const obj = new Example(element);
-  // obj.init();
+  // const element = document.getElementById('container');
 };
