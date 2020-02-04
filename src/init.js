50adb077c7f13a2e38526d484664e624f6b342e8
Vasilek <morphizm@ya.ru>
2020-02-04 20:58:31 +0300

    yup, view

--- src/init.js
+++ src/init.js
@@ -1,78 +1,92 @@
 import '@babel/polyfill';
 import 'bootstrap';
 import axios from 'axios';
-import WatchJS from 'melanke-watchjs';
-import validator from 'validator';
+import { string } from 'yup';
+import _ from 'lodash';
+import { watch } from 'melanke-watchjs';
+
 import './styles/custom.scss';
 import parse from './parsers/rssparser';
-import render, { renderValidBorder } from './renders/render';
+import render from './view';
 
-// setAttribute('disabled', true);
 const doRequest = (url, proxy) => {
   return axios.get(`https://${proxy}/${url}`);
 };
 
+// Предпочитайте композицию (пайплайн) вместо матрешки функций.
+// valid, invalid, blank
 export default () => {
-  const state = {
-    feedList: ['a.ru'],
-    currentURL: null,
-    isValidURL: false,
+  const initLink = {
+    id: _.uniqueId(),
+    url: 'http://a.ru',
+    title: 'My title',
+    description: 'My description',
+    items: [],
   };
-  const container = document.querySelector('.container');
-  const content = document.querySelector('.content');
-  const form = container.querySelector('form');
-  const input = form.querySelector('input');
 
+  const state = {
+    feedList: [initLink],
+    currentURL: '',
+    urlState: 'blank',
+    errors: {},
+  };
+
+  // const repl = 'repl.it/@enmalafeev/RSS-reader'
   const proxy = 'cors-anywhere.herokuapp.com';
   const loremRss = 'lorem-rss.herokuapp.com/feed';
-  // const repl = 'repl.it/@enmalafeev/RSS-reader'
 
-  const handleChange = (e) => {
-    state.currentURL = e.target.value;
+  const elements = {
+    container: document.querySelector('.container'),
+    content: document.querySelector('.content'),
+    form: document.querySelector('form'),
+    urlInput: document.querySelector('input'),
   };
-  input.addEventListener('change', handleChange);
-  const { watch } = WatchJS;
 
-  watch(state, 'currentURL', () => {
-    const isValidURL = validator.isURL(state.currentURL);
-    const hasFeedListURL = state.feedList.every((e) => e !== state.currentURL);
-    renderValidBorder(input, isValidURL && hasFeedListURL);
-  });
+  const handleChange = ({ target: { value } }) => {
+    state.currentURL = value;
+    if (value === '') {
+      state.urlState = 'blank';
+      return;
+    }
+
+    const schema = string().url();
+    const hasFeedListURL = state.feedList.every((l) => l.url !== state.currentURL);
+    schema.isValid(state.currentURL)
+      .then((result) => {
+        state.urlState = result && hasFeedListURL ? 'valid' : 'invalid';
+      });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('SUBMIT')
-    const { feedList, currentURL, isValidURL } = state;
-    if (isValidURL) {
+    const { feedList, currentURL, urlState } = state;
+    if (urlState !== 'valid') {
       return;
     }
-    let isLoading = true;
 
     doRequest(currentURL, proxy)
       .then(() => {
 
-      })
-      // eslint-disable-next-line no-console
-      .catch(console.log)
-      .finally(() => { isLoading = false; });
+      });
   };
 
-  form.addEventListener('submit', handleSubmit);
+  elements.urlInput.addEventListener('input', handleChange);
+  elements.form.addEventListener('submit', handleSubmit);
 
   const domparser = new DOMParser();
 
-  // let isLoading = true;
   doRequest(loremRss, proxy)
     .then((e) => {
-      // console.log(e);
       const { data } = e;
-      // console.log(data);
       const parsedData = domparser.parseFromString(data, 'text/xml');
-      // console.log(parsedData);
-      // console.log(parse(parsedData));
-      render(content, parse(parsedData));
-    })
-    // eslint-disable-next-line no-console
-    .catch(console.log)
-    // .finally(() => { isLoading = false; });
+      const p = parse(parsedData);
+      const newFeed = {
+        id: _.uniqueId(),
+        ...parse(parsedData),
+      };
+      state.feedList.push(newFeed);
+      console.log(p);
+    });
+
+  render(elements, state);
 };
---
+++ src/view/index.js
@@ -0,0 +1,69 @@
+import { watch } from 'melanke-watchjs';
+
+const renderItems = (items) => {
+  const result = items.map((i) => {
+    const { text, link } = i;
+    const html = `
+      <li class="list-group-item">
+        <a href="${link}">${text}</a>
+      </li>
+    `;
+    return html;
+  }).join('');
+
+  return `
+    <ul class="list-group list-group-flush">
+      ${result}
+    </ul>
+  `;
+};
+
+const render = (elements, state) => {
+  // console.log('R')
+  const {
+    content, urlInput,
+  } = elements;
+
+  watch(state, 'isValidUrl', () => {
+    const { urlState } = state;
+    switch (urlState) {
+      case 'valid': {
+        urlInput.classList.add('is-valid');
+        urlInput.classList.remove('is-invalid');
+        break;
+      }
+      case 'invalid': {
+        urlInput.classList.add('is-invalid');
+        urlInput.classList.remove('is-valid');
+        break;
+      }
+      default: {
+        urlInput.classList.remove('is-valid');
+        urlInput.classList.remove('is-invalid');
+      }
+    }
+  });
+
+  watch(state, 'feedList', () => {
+    state.feedList.forEach((l) => {
+      const { title, description, items } = l;
+      const div = document.createElement('div');
+      div.classList.add('card');
+      const text = `
+        <div class="card mb-0">
+          <div class="card-header">
+            ${title}
+          </div>
+          <div class="card-body pb-1">
+            <p class="card-text">${description}</p>
+            ${renderItems(items)}
+          </div>
+        </div>
+      `;
+      div.innerHTML = text;
+      content.prepend(div);
+    });
+  });
+};
+
+export default render;
